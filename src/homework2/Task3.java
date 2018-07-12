2f654f3871ebfe4c3000279bd4c364f6ba9e982c
Andrew Olkhovsky <andrew.olkhovsky@gmail.com>
2018-07-12 14:03:34 +0300

    implemented, but incorrect need debugging

--- src/homework2/Task3.java
+++ src/homework2/Task3.java
@@ -28,6 +28,42 @@ public class Task3 {
     }
 
     public static String solution(String index) {
-        return String.format("Solved(%s)", index);
+        // 1. describe the sequence
+        // 2. is $index element in sequence ?
+        // 3. binsearch by hypotetical number of $index element in sequence:
+        //    if found -> return 1
+        //    not found -> return 0
+        int intIndex = Integer.parseInt(index);
+        int result = binSearch(intIndex, 0, intIndex / 2);
+
+        if (result == -1) {
+            return "0";
+        }
+        return "1";
+    }
+
+    public static int binSearch(int searchElem, int lo, int hi) {
+        if (lo < hi) {
+            int mid = (lo / 2) + (hi / 2);
+            int midElem = sequenceFunc(mid);
+
+            if (midElem < searchElem) {
+                // left
+                return binSearch(searchElem, lo, mid - 1);
+            }
+            if (midElem > searchElem) {
+                return binSearch(searchElem, mid + 1, hi);
+            }
+
+            return mid;
+        }
+        if (sequenceFunc(lo) == searchElem) {
+            return lo;
+        }
+        return -1;
+    }
+
+    public static int sequenceFunc(int x) {
+        return ((x * (x+1)) / 2) + 1;
     }
 }
