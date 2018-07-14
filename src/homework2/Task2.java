02d2831a7ba2c87cb256f35ac21dd4c85efbce4d
Andrew Olkhovsky <andrew.olkhovsky@gmail.com>
2018-07-14 22:58:39 +0300

    implemented

--- src/homework2/Task2.java
+++ src/homework2/Task2.java
@@ -1,16 +1,45 @@
 package homework2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 @SuppressWarnings("WeakerAccess")
 class Task2 {
     public static void main(String... args) {
-        double a = 15;
-        double n = 7;
+        Scanner scan = new Scanner(System.in);
+        double[] raw = Arrays.stream(scan.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
 
-        double lastResult = 9.75;
-        for (double i = n; i > 0; i--) {
-            lastResult = calculateAn(lastResult, i, 15);
-            System.out.println(lastResult);
+        double n = raw[0];
+        double a = raw[1];
+
+        solution(n, a);
+    }
+
+    static void solution(double n, double a) {
+        double l = 0;
+        double r = 100000000;
+
+        for (int i = 0; i < 1000; i++) {
+            double m = (l + r) / 2;
+            if (minPosition(m, n - 1, a) >= 0) {
+                r = m;
+            } else {
+                l = m;
+            }
         }
+
+        System.out.println(r);
+    }
+
+    static double minPosition(double m, double n, double a) {
+        double min = Double.MAX_VALUE;
+
+        for (double i = n; i > 0; i--) {
+            m = calculateAn(m, i, a);
+            min = Double.min(min, m);
+        }
+
+        return min;
     }
 
     static double calculateAn(double aNext, double n, double a) {
