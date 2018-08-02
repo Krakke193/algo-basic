ecce7b9b1ff56e4601d8984d50549268dc931cdf
Andrii Olkhovskyi <andrii.olkhovskyi@betlab.com>
2018-08-02 10:15:26 +0300

    2 spaces indentation

--- src/homework4/BinaryStrings.ts
+++ src/homework4/BinaryStrings.ts
@@ -1,27 +1,28 @@
 function processData(input: string) {
-    const byLines = input.split("\n");
-    const [n, zeros, ones] = byLines[0].split(" ");
+  const byLines = input.split("\n");
+  const [n, zeros, ones] = byLines[0].split(" ");
 
-    const lines = byLines.slice(1);
+  const lines = byLines.slice(1);
 
-    lines.forEach(line => {
-        const [zeros, ones] = countNumbers(line);
-        console.log(`line: ${line}. zeros:${zeros}|ones:${ones}`);
-    });
+  lines.forEach(line => {
+    const [zeros, ones] = countNumbers(line);
+    console.log(`line: ${line}. zeros:${zeros}|ones:${ones}`);
+  });
 }
 
+
 function countNumbers(line: string): [number, number] {
-    let numRepr = line.split('').map(str => parseInt(str));
-    let zeros = 0;
-    let ones = 0;
+  let numRepr = line.split('').map(str => parseInt(str));
+  let zeros = 0;
+  let ones = 0;
 
-    numRepr.forEach(num => {
-        num == 0
-            ? zeros++
-            : ones++;
-    });
+  numRepr.forEach(num => {
+    num == 0
+      ? zeros++
+      : ones++;
+  });
 
-    return [zeros, ones];
+  return [zeros, ones];
 }
 
 const data = '3 3 1\n1\n00\n100';
